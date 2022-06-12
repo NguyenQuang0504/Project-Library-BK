901e5b78741178ae6ad55185229aef96af49b8cc
NguyenQuang0504 <quang10a30504@gmail.com>
2022-06-12 17:15:22 +0700

    update

--- src/main/java/quan_ly_thu_vien/demo/controller/RFIDController.java
+++ src/main/java/quan_ly_thu_vien/demo/controller/RFIDController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import quan_ly_thu_vien.demo.model.StudentId;
@@ -14,7 +15,7 @@ public class RFIDController {
     @Autowired
     private IStudentId iStudentId;
 
-    @GetMapping("/rfid")
+    @PostMapping("/rfid")
     public ResponseEntity<StudentId> getRFID(@RequestParam String ID){
         StudentId student = new StudentId();
         student.setIdCard(ID);
